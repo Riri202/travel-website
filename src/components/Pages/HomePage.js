703dcea7a12a9fd739b4ba4473b4df23b67791e8
Rita-Maria Oladokun <Rita-Maria@Rita-Marias-MacBook-Pro.local>
2022-01-19 21:18:00 +0100

    initial commit

---
+++ src/components/Pages/HomePage.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import '../../App.css'
+import HeroSection from '../HeroSection'
+
+
+function HomePage() {
+    return (
+        <div>
+            <HeroSection/>
+        </div>
+    )
+}
+
+export default HomePage;
